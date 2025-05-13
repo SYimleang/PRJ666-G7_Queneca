d577bcd9486f3192796b91511afe7e8d72ebeff4
Sasawat Yimleang <syimleang@SYs-MBA.local>
2025-05-12 23:53:52 -0400

    Update directory

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/admin/page.tsx
-export default function AdminDashboardPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-semibold mb-4">Welcome, Admin</h1>
-      <p className="text-gray-600">Heres your dashboard overview.</p>
-    </div>
-  );
-}
