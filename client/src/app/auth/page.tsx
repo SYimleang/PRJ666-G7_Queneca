211e189647a66ba278a93c5acef1e50cbc7cd42f
MarcusB71 <marcus.brown1234567@gmail.com>
2025-05-20 14:32:18 -0400

    add auth page to render login/signup form

---
+++ client/src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '@/components/AuthForm';
+
+export default function AuthPage() {
+  return (
+    <div className="min-h-screen flex justify-center">
+      <AuthForm />
+    </div>
+  );
+}
