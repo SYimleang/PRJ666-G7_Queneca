fe38dc41e0a8002bdea697d889589622d4f461ca
MarcusB71 <marcus.brown1234567@gmail.com>
2025-05-29 14:29:22 -0400

    display username instead of email

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -84,7 +84,7 @@ export default function Navbar() {
                 onClick={() => setOpen(!open)}
                 className="bg-red-500 text-white px-4 py-2 rounded"
               >
-                {user.email}
+                {user.username}
               </button>
 
               {open && (
