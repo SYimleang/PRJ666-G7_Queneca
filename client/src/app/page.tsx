309241777022efb0a4d38b2b211263d2e4eca2e7
MarcusB71 <marcus.brown1234567@gmail.com>
2025-06-10 16:37:21 -0400

    suggested edits, reference competitors

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function AuthPage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1>App landing page here!</h1>
+      <div>
+        <h1>App landing page here!</h1>
+        <h1>Possible landing page options:</h1>
+        <h1>Carousel with screenshots of staff, admin, customer views,</h1>
+        <h1>Why Queneca</h1>
+        <h1>Features</h1>
+        <h1>Testimonials</h1>
+      </div>
     </main>
   );
 }
