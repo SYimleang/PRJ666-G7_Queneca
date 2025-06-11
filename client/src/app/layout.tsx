4e9651b4d845ee19eed12a09b7a9ed27f80e45d7
Sasawat Yimleang <framilly.eng@gmail.com>
2025-06-11 16:03:20 -0400

    Add Footer

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import { UserProvider } from '../context/UserContext';
 import { RestaurantProvider } from '../context/RestaurantContext';
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           <RestaurantProvider>
             <Navbar />
             <main className="pt-4 px-2">{children}</main>
+            <Footer />
           </RestaurantProvider>
         </UserProvider>
       </body>
