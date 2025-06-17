6a462634ec549213a259c79821e596e12683f9b9
MarcusB71 <marcus.brown1234567@gmail.com>
2025-06-17 14:08:00 -0400

    add bg color to whole page

--- client/src/app/auth/page.tsx
+++ client/src/app/auth/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "@/components/AuthForm";
 
 export default function AuthPage() {
   return (
-    <div className="min-h-screen flex justify-center">
+    <div className="min-h-screen flex justify-center bg-gradient-to-br from-red-50 to-orange-50">
       <AuthForm />
     </div>
   );
