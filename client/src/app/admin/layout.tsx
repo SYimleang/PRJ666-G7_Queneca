a7cd01f82887cff6f4480a2c76a3e50e85e8cc14
Sasawat Yimleang <framilly.eng@gmail.com>
2025-06-19 13:22:41 -0400

    Update authorization of admin routes

---
+++ client/src/app/admin/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+"use client";
+
+import { useUser } from "@/context/UserContext";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Not logged in
+    if (!user) {
+      router.replace("/login");
+    } 
+    // Staff
+    else if (user.role == "staff") {
+      router.replace("/staff");
+    } 
+    // Customer
+    else if (user.role == "customer") {
+        router.replace("/");
+    }
+  }, [user, router]);
+
+  if (!user || user.role !== "admin") return null;
+
+  return <>{children}</>;
+}
