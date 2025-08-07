0c4298fd47887a8ee4e51ae40840ca8c2c0f8b35
Sasawat Yimleang <framilly.eng@gmail.com>
2025-08-07 16:54:54 -0400

    Add Suspense boundary

--- client/src/app/auth/page.tsx
+++ client/src/app/auth/page.tsx
@@ -1,9 +1,12 @@
 import AuthForm from "@/components/AuthForm";
+import { Suspense } from "react";
 
 export default function AuthPage() {
   return (
     <div className="min-h-screen flex justify-center bg-gradient-to-br from-red-50 to-orange-50">
-      <AuthForm />
+      <Suspense>
+        <AuthForm />
+      </Suspense>
     </div>
   );
 }
